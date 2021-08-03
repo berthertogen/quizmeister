6bb007d0905f3dcb094fae49b8f6078278be8192
Bert Hertogen <bert@hertogen.net>
2021-08-03 12:08:44 +0200

    Hello world

---
+++ service/Api/HelloWorldController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Quizmeister
+{
+
+    [ApiController]
+    [Route("[controller]")]
+    public class HelloWorldController : ControllerBase
+    {
+
+        [HttpGet]
+        public ActionResult Get(int id)
+        {
+            return Ok("Hello world");
+        }
+    }
+}
